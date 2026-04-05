6332250dd3edd44061e595bcc7976ea3927e0147
Laila Seif <lailaseif710@gmail.com>
2026-04-05 15:52:06 -0400

    install laravel backend setup (cake and flavour files)
    i installed Laravel in the backend folder and used the artisan commands we practiced in class to create the files i need for my two resource types
    
    cake and flavour
    
    this lets me set up the structure needed to start building the api
    
    laravel created many files!

---
+++ backend/database/factories/FlavourFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Flavour;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Flavour>
+ */
+class FlavourFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
