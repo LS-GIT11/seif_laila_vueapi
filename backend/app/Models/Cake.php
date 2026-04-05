d6620443fbaaa7ad90a2f2ee6d60e8f24b87290b
Laila Seif <lailaseif710@gmail.com>
2026-04-05 16:54:32 -0400

    adding my cake and flavour model relationships
    i set up the relationship between my cake and flavour models based on how we in class connected book and author ... so each cake belongs to a flavour and each flavour can have multiple cakes

--- backend/app/Models/Cake.php
+++ backend/app/Models/Cake.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Cake extends Model
 {
     /** @use HasFactory<\Database\Factories\CakeFactory> */
     use HasFactory;
+
+    public function flavour(): BelongsTo
+    {
+        return $this->belongsTo(Flavour::class);
+    }
 }
--- backend/app/Models/Flavour.php
+++ backend/app/Models/Flavour.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Flavour extends Model
 {
     /** @use HasFactory<\Database\Factories\FlavourFactory> */
     use HasFactory;
+
+    public function cakes(): HasMany
+    {
+        return $this->hasMany(Cake::class);
+    }
 }
